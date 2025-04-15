71f4708602c7a248e38e70f1b8990bbded5735bf
ssalo <shdzsalo@gmail.com>
2025-04-15 08:36:56 +0300

    complete task 1

--- I_Flave_Task.py
+++ I_Flave_Task.py
@@ -6,17 +6,44 @@ def josephus (n,k):
     for i in range(1,n+1):
         arr.append(i)
 
+    index = 0
+
     while(len(arr)>0):
-        i = 0
-        m = (n+1) // k
-        for j in range(1,m):
-            res.append (arr[(j-1)*k])
+
+        
+
+        if k > len(arr):
+            arr_copy = arr.copy()
+            while (k//(len(arr)+1)) > 0:
+                arr.extend(arr_copy)
+            
+
+        m = (len(arr)) // k
+        
+        for j in range(0, m):
+            index = (j+1) * k -1
+
+            if arr[index] not in res:
+                    res.append(arr[index])
+
+
+        new_arr = []
+        for l in range(arr.index(res[-1]) + 1, len(arr)):
+            if arr[l] not in new_arr and arr[l] not in res:
+                new_arr.append(arr[l])
 
         for j in res:
-            arr.remove(j)
+            if j in arr:
+                arr.remove(j)
+        
+        for g in arr:
+            if g not in new_arr and g not in res:
+                new_arr.append(g)
+
+        arr = new_arr
         
     return res
 
-res = josephus(6,2)
+result = josephus(4,3)
 
-print(res)
+print(result)
